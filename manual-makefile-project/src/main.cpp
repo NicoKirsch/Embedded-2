110c0b996b0ffa5cc1da58cb4766980eecadd001
NicoKirsch <kirschnico03@gmail.com>
2026-04-22 22:10:25 +0200

    änderung zu mock Datei

--- manual-makefile-project/src/main.cpp
+++ manual-makefile-project/src/main.cpp
@@ -7,29 +7,28 @@ int main() {
     Heartbeat heart;
     
     // Watchdog initialisieren
-    // Beispiel: 1s
-    heart.init(1000);
+    // Beispiel: 200ms
+    heart.init(200);
 
     int loopCounter = 0; // Zähler für die Anzahl der Schleifendurchläufe
-    int Pinx = 13; // Beispiel Pin für LED
     // watchdog parallel zum Hauptprogramm laufen lassen
 
     while (true) {
-        digitalWrite(Pinx,1);
-        delay_ms(1);
+        HAL_GPIO_WritePin(LEDPIN_GPIO_Port, LEDPIN_Pin, GPIO_PIN_SET); // digitalWrite(Pinx,1);
+        HAL_Delay(1);
         
         // Watchdog füttern
         heart.tick();
 
-        digitalWrite(Pinx,0);
-        delay_ms(1);
+        HAL_GPIO_WritePin(LEDPIN_GPIO_Port, LEDPIN_Pin, GPIO_PIN_RESET); //  digitalWrite(Pinx,0);
+        HAL_Delay(1);
 
         //Watchdog füttern
         heart.tick();
 
         loopCounter++;
         if (loopCounter == 10) { // Nach 10 Schleifendurchläufen den Watchdog nicht mehr füttern
-            delay_ms(2000); // Simuliere eine Verzögerung, die den Watchdog auslöst
+            HAL_Delay(2000); // Simuliere eine Verzögerung, die den Watchdog auslöst
         }
     }
 
